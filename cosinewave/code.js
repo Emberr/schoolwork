41bda38f7bf52d64767cca676908b5d78feea7e5
Ember <embershotss@gmail.com>
2022-02-19 14:57:16 +0100

    big

--- cosinewave/code.js
+++ cosinewave/code.js
@@ -3,16 +3,20 @@ let context = canvas.getContext("2d")
 
 let timer = 0
 function drawFrame() {
-context.clearRect(0,0,600,600)
+context.clearRect(0,0,1000,1000)
 context.lineWidth = 1
+
+context.strokeStyle = "#c1b492"
 context.beginPath()
 
-for (let y=0;y<600;y+=10) {
+for (let y=0;y<1000;y+=10) {
     let y2 = Math.cos((timer+y)/300)*200+200
     context.moveTo(0,y2)
-    context.lineTo(600,y2)
     
+    context.lineTo(1080,y2)
 }
+
+
 context.stroke()
 timer += 1
 }
